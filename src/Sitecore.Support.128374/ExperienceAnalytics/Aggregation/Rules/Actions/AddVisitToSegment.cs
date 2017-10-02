9d2eac936be5b0e9230de46695fd08a363c38263
Dmitry Baranov <dbr@sitecore.net>
2017-10-02 19:06:40 +0300

    Default version.

---
+++ src/Sitecore.Support.128374/ExperienceAnalytics/Aggregation/Rules/Actions/AddVisitToSegment.cs
@@ -0,0 +1,14 @@
+﻿namespace Sitecore.Support.ExperienceAnalytics.Aggregation.Rules.Actions
+{
+    using Sitecore.Rules;
+    using Sitecore.Rules.Actions;
+
+    public class AddVisitToSegment<T> : RuleAction<T>
+      where T : RuleContext
+    {
+        public override void Apply(T ruleContext)
+        {
+            ruleContext.Parameters.Add("addVisit", true);
+        }
+    }
+}
